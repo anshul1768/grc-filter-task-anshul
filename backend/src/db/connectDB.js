13520aefb687db8968fa985e0d71f2a6d1a11d68
Anshul <devaksh2211@gmail.com>
2026-02-10 12:05:54 +0530

    handled db error at locahost and git clone

--- backend/src/db/connectDB.js
+++ backend/src/db/connectDB.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async() => {
   try {
-    let connect = await mongoose.connect(process.env.MONGODB_URI);
+    let connect = await mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://iamanshul2211_db_user:anshul9542@cluster0.1dde712.mongodb.net/');
     console.log("MongoDB Database connected successfully");
   } catch (error) {
     console.log("MongoDB error", error);
